a6ca8cc9b1f074a68612076ebc35ac2b93f4d564
heiwa4126 <heiwa4126@gmail.com>
2024-10-25 13:40:19 +0900

    add routing

--- src/Utils.tsx
+++ src/Utils.tsx
@@ -1,17 +1,18 @@
 export function OpenInNewWindow() {
+	const currentLocation = window.location.href;
 	return (
 		<>
 			<a
-				href="."
+				href={currentLocation}
 				onClick={(e) => {
 					e.preventDefault(); // デフォルトのリンク動作を防ぐ
-					window.open(e.currentTarget.href, "_blank", "width=800,height=600");
+					window.open(currentLocation, "_blank", "width=800,height=600");
 				}}
 			>
 				このページを別のウインドウで開く
 			</a>
 			{" | "}
-			<a href="." target="_blank" rel="noopener noreferrer">
+			<a href={currentLocation} target="_blank" rel="noopener noreferrer">
 				このページを別のタブで開く
 			</a>
 		</>
